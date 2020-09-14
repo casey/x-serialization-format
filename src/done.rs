5851efe1f28aba0d01132652c5129735077876ce
Casey Rodarmor <casey@rodarmor.com>
2020-09-13 22:06:43 -0700

    Add inline attribute to suppress compiler ICE

--- src/done.rs
+++ src/done.rs
@@ -4,6 +4,10 @@ use crate::common::*;
 pub struct Done;
 
 impl Done {
+  // This inline attribute is required to avoid a compiler ICE as of rustc
+  // 1.48.0-nightly (a1947b3f9 2020-09-10). It should be removed once the ICE is
+  // fixed.
+  #[inline(always)]
   pub fn done(self) {}
 }
 
